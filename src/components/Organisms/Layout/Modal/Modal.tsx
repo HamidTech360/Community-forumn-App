94c96bd932d00ecf3a1278d2afdd7bdb452e67b6
John Barnes Oduro Twumasi <ohenesetwumasi@gmail.com>
2022-04-26 15:23:23 +0000

    Fix: Feed fixed

--- src/components/Organisms/Layout/Modal/Modal.tsx
+++ src/components/Organisms/Layout/Modal/Modal.tsx
@@ -1,5 +1,6 @@
+import { useRouter } from "next/router";
 import React from "react";
-import { Col, Image, Modal as M, Row } from "react-bootstrap";
+import { Button, Col, Image, Modal as M, Row } from "react-bootstrap";
 import { useModalWithData } from "../../../../hooks/useModalWithData";
 const Modal = ({
   body,
@@ -10,8 +11,18 @@ const Modal = ({
   show: boolean;
   close: () => void;
 }) => {
+  const router = useRouter();
   return (
     <M size="xl" centered show={show} onHide={close}>
+      <Button
+        variant="light"
+        size="lg"
+        style={{ fontSize: "1.5rem" }}
+        onClick={() => close()}
+        className="position-fixed"
+      >
+        &larr;
+      </Button>
       <M.Body className="show-grid">{body}</M.Body>
     </M>
   );
