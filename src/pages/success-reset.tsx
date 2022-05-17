7b918d51810d5cd94033dd3360c5cc5a556c9743
unknown <stella.oseyomon@gmail.com>
2022-05-18 00:18:46 +0100

    success-reset

--- src/pages/success-reset.tsx
+++ src/pages/success-reset.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import styles from "../styles/success-reset.module.scss";
 
 const SuccessReset = () => {
-  return <div>SuccessReset</div>;
+  return ( 
+    <>
+      <div className = {styles.body}>
+        <div className = {styles.page}>
+          <h1>Success!</h1>
+          <p>Your new password has been created</p>
+
+          <button>Back To Login</button>
+        </div>
+      </div>
+    </>
+  )
 };
 
 export default SuccessReset;
