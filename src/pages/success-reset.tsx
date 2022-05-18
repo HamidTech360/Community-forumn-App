1d924fbb0f8bc8c33ffe074424b8549e30e095b4
unknown <stella.oseyomon@gmail.com>
2022-05-18 17:31:13 +0100

    assets

--- src/pages/success-reset.tsx
+++ src/pages/success-reset.tsx
@@ -1,16 +1,28 @@
 import React from "react";
+import { Image } from 'react-bootstrap'
 import styles from "../styles/success-reset.module.scss";
 
 const SuccessReset = () => {
   return ( 
     <>
       <div className = {styles.body}>
+        <Image
+          src="/assets/ellipse-intro-top.png"
+          className={styles.vector1}
+          alt=""
+        />
+
         <div className = {styles.page}>
           <h1>Success!</h1>
           <p>Your new password has been created</p>
 
           <button>Back To Login</button>
         </div>
+          <Image
+          src="/assets/ellipse-intro-right.png"
+          className={styles.vector2}
+          alt=""
+        />
       </div>
     </>
   )
