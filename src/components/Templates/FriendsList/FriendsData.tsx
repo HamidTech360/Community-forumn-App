ca8ea52a1430b4b65242f5f6747db72f40fe05ad
Stella Oseyomon <78904383+stellaose@users.noreply.github.com>
2022-05-20 08:28:06 +0100

    friends

---
+++ src/components/Templates/FriendsList/FriendsData.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Badge, Card, Col, Image, ListGroup, Row } from 'react-bootstrap';
+import styles from '../../../styles/friends.module.scss';
+
+interface FriendsListProp {
+    friendsList: FriendsDataType
+  }
+  
+  export type FriendsDataType = {
+    friends : friendType[]
+    
+  }
+
+  type friendType = {
+    id: number,
+    image: string,
+    name: string,
+    number: string,
+    menu: string
+  }
+
+const FriendsData = (props: FriendsListProp) => {
+    
+    const { friends } = props.friendsList;
+    console.log(friends)
+
+  return (
+      <>
+            <Row xs={1} md={2} className="g-4" >
+
+                {friends.map((friend) => (
+                    <Col md={12} lg = {6}>
+                        <Card key= { friend.id} className = {styles.card}>
+                            <Card.Body >
+                                <ListGroup variant = 'flush'>
+                                    <ListGroup.Item  as="li" className="d-flex justify-content-between align-items-start">
+                                        <div>
+                                            <Image src= {friend.image} alt='debola'/>
+                                        </div>
+                                        <div className = { styles.title}>
+                                                                                                        <Card.Title className = { styles.name}>
+                                                {friend.name}
+                                            </Card.Title>
+                                            <Card.Subtitle className = "mb-2 text-muted">{friend.number} </Card.Subtitle>
+
+                                        </div>
+
+                                        <Badge bg= 'light'>
+                                            <Image src = {friend.menu} alt = 'dot'/>
+                                        </Badge>
+                                    </ListGroup.Item>
+                                </ListGroup>
+                                                              
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    
+                ))}
+        </Row>
+      </>
+    
+  )
+}
+
+export default FriendsData
