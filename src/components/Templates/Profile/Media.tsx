8238494531ba1dde28911356d49a7d863b24cfd5
HamidTech360 <hammedowolabi2001@gmail.com>
2022-05-20 09:23:13 -0700

    adding the media template

--- src/components/Templates/Profile/Media.tsx
+++ src/components/Templates/Profile/Media.tsx
@@ -1,7 +1,42 @@
 import React from "react";
+import styles from '../../../styles/mediaTemplate.module.css'
 
 const Media = () => {
-  return <div>Media</div>;
+  const imageList = [
+    {
+      image:'imagePlaceholder.jpg'
+    },
+    {
+      image:'imagePlaceholder.jpg'
+    },
+    {
+      image:'imagePlaceholder.jpg'
+    },
+    {
+      image:'imagePlaceholder.jpg'
+    },
+    {
+      image:'imagePlaceholder.jpg'
+    },
+    {
+      image:'imagePlaceholder.jpg'
+    },
+  ]
+  return (
+    <div>
+      <div className={styles.mediaImages}>
+          <div className={styles.imageGrid}>
+             {
+               imageList.map((item, i)=>
+               <div className={styles.imageGridItem}>
+                  <img src={`/images/${item.image}`} className={styles.image} alt="placeholder" />
+                </div>
+               )
+             }
+          </div>
+      </div>
+    </div>
+  )
 };
 
 export default Media;
