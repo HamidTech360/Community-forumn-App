0209530041477f72135cf9c4aa32226668cc69cb
Sidodus <saheedodulaja@gmail.com>
2022-05-27 08:53:34 +0100

    View Blog Post

---
+++ src/components/BlogPost/dummyData.js
@@ -0,0 +1,214 @@
+export const dummyData = {
+  author: "Mike Dike",
+  title: "5 ways to work abroad",
+  authorImage: "/images/friends1.png",
+  blogImage: "/assets/blog-post/pana.svg",
+  time: new Date(), //.toLocaleString(),
+  keywords: ["Work abroad", "Canada", "Housing in Canada"],
+  article: (
+    <div>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci,
+        tempora.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam,
+        praesentium vitae? Voluptatum, eos fugit asperiores quo quaerat fuga
+        autem quae nobis ab magnam nemo, esse ea ipsam est ratione nam?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur,
+        recusandae totam? Perferendis animi odio expedita corporis nulla
+        repudiandae, unde, rerum quos fugiat quis est provident?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam minus
+        inventore enim ad vitae unde error sed et facilis explicabo molestias
+        modi exercitationem suscipit ut corrupti sapiente ipsa dicta illo, nobis
+        velit, nulla eum quos placeat. Maxime, magni? Adipisci incidunt illo id
+        officiis reprehenderit vero repellat explicabo laudantium exercitationem
+        laboriosam?
+      </p>
+      <p>Lorem ipsum dolor sit amet.</p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore
+        reiciendis blanditiis quas at tempora? Ipsum, delectus laudantium,
+        corrupti est aliquid optio saepe minima nemo libero deleniti rem ratione
+        eum impedit et obcaecati, excepturi dignissimos? Vel magni assumenda,
+        possimus, modi maiores illo labore consequatur debitis quaerat earum,
+        libero facere autem alias doloremque reiciendis. Rem autem ipsum numquam
+        voluptate enim a voluptatum impedit est odio accusantium ex expedita,
+        quae mollitia totam, corporis sed perferendis provident voluptatibus
+        facilis id aliquam fuga architecto. Dolor quam harum veritatis esse sint
+        quisquam quibusdam fuga. Corporis delectus cupiditate quas eius
+        molestiae in eveniet dolor blanditiis?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque saepe,
+        eos illum eius ratione natus harum perferendis quis numquam optio
+        tenetur necessitatibus obcaecati magnam recusandae, minima nam
+        architecto quibusdam vitae a, iusto odit culpa eaque! Odio consequuntur
+        eaque maxime earum amet accusantium quas natus, quo velit illo excepturi
+        dicta nulla, pariatur facilis eligendi neque iure dolores. Unde
+        cupiditate, saepe vel veniam, repudiandae qui exercitationem harum sit
+        praesentium id officia enim est neque ratione ipsam voluptatibus
+        nesciunt.
+      </p>
+    </div>
+  ),
+  comments: [
+    {
+      name: "Ephraim Nkonam",
+      image: "/images/friends2.png",
+      time: new Date(),
+      comment:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, error? ",
+      like: [],
+      reply: [
+        {
+          name: "",
+          image: "/images/friends4.png",
+          reply: "",
+          time: "",
+        },
+      ],
+    },
+    {
+      name: "Philips Thomas",
+      image: "/images/friends5.png",
+      time: new Date(),
+      comment:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe quidem ullam tempore libero nobis quod unde dolor vitae qui totam.",
+      like: [
+        {
+          name: "Andrew Brew",
+          image: "/images/friends6.png",
+        },
+      ],
+      reply: [],
+    },
+    {
+      name: "Jackson Wright",
+      image: "/images/friends7.png",
+      time: new Date(),
+      comment:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis iste corrupti cumque sit, ratione amet repellendus!",
+      like: [
+        {
+          name: "Andrew Brew",
+          image: "/images/friends8.png",
+        },
+      ],
+      reply: [
+        {
+          name: "Mike Dike",
+          image: "/images/friends9.png",
+          reply: "Lorem ipsum dolor sit amet.",
+          time: new Date(),
+        },
+      ],
+    },
+    {
+      name: "Ephraim Nkonam",
+      image: "/images/friends2.png",
+      time: new Date(),
+      comment:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, error? ",
+      like: [],
+      reply: [
+        {
+          name: "",
+          image: "/images/friends4.png",
+          reply: "",
+          time: "",
+        },
+      ],
+    },
+    {
+      name: "Philips Thomas",
+      image: "/images/friends5.png",
+      time: new Date(),
+      comment:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe quidem ullam tempore libero nobis quod unde dolor vitae qui totam.",
+      like: [
+        {
+          name: "Andrew Brew",
+          image: "/images/friends6.png",
+        },
+      ],
+      reply: [],
+    },
+    {
+      name: "Jackson Wright",
+      image: "/images/friends7.png",
+      time: new Date(),
+      comment:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis iste corrupti cumque sit, ratione amet repellendus!",
+      like: [
+        {
+          name: "Andrew Brew",
+          image: "/images/friends8.png",
+        },
+      ],
+      reply: [
+        {
+          name: "Mike Dike",
+          image: "/images/friends9.png",
+          reply: "Lorem ipsum dolor sit amet.",
+          time: new Date(),
+        },
+      ],
+    },
+    {
+      name: "Ephraim Nkonam",
+      image: "/images/friends2.png",
+      time: new Date(),
+      comment:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, error? ",
+      like: [],
+      reply: [
+        {
+          name: "",
+          image: "/images/friends4.png",
+          reply: "",
+          time: "",
+        },
+      ],
+    },
+    {
+      name: "Philips Thomas",
+      image: "/images/friends5.png",
+      time: new Date(),
+      comment:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe quidem ullam tempore libero nobis quod unde dolor vitae qui totam.",
+      like: [
+        {
+          name: "Andrew Brew",
+          image: "/images/friends6.png",
+        },
+      ],
+      reply: [],
+    },
+    {
+      name: "Jackson Wright",
+      image: "/images/friends7.png",
+      time: new Date(),
+      comment:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis iste corrupti cumque sit, ratione amet repellendus!",
+      like: [
+        {
+          name: "Andrew Brew",
+          image: "/images/friends8.png",
+        },
+      ],
+      reply: [
+        {
+          name: "Mike Dike",
+          image: "/images/friends9.png",
+          reply: "Lorem ipsum dolor sit amet.",
+          time: new Date(),
+        },
+      ],
+    },
+  ],
+};
