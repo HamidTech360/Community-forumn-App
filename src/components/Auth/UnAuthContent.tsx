7baa2e4ab8460a7fd34859981daec1349d79bcb5
John Barnes Oduro Twumasi <ohenesetwumasi@gmail.com>
2022-05-27 18:19:17 +0000

    Feat: UnAuth Component

---
+++ src/components/Auth/UnAuthContent.tsx
@@ -0,0 +1,22 @@
+import { useRouter } from "next/router";
+import { ReactNode, useEffect } from "react";
+import useUser from "../../hooks/useUser";
+import Loader from "../Organisms/Layout/Loader/Loader";
+
+export default function UnAuthContent({ children }: { children: ReactNode }) {
+  const { isAuthenticated, authenticating } = useUser();
+  const router = useRouter();
+
+  // Navigate unauthenticated users to Log In page.
+  useEffect(() => {
+    if (!authenticating && isAuthenticated) {
+      router.push("/feed");
+    }
+  }, [isAuthenticated, authenticating, router]);
+
+  if (isAuthenticated) {
+    return <div>{children}</div>;
+  }
+
+  return <Loader />;
+}
