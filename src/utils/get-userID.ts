6dc0596d7b36ec8c73c49efaf150fa6c9dae00a3
HamidTech360 <hammedowolabi2001@gmail.com>
2022-06-06 14:47:26 -0700

    adding the post handler

--- src/utils/get-userID.ts
+++ src/utils/get-userID.ts
@@ -3,6 +3,8 @@ import { verify } from "jsonwebtoken";
 const getUserID = (token: string) => {
   try {
     const payload: any = verify(token, process.env.JWT_SECRET || "");
+    console.log(`payload is ${payload.sub}`);
+    
     return payload.sub;
   } catch (error) {
     if (error instanceof Error) {
