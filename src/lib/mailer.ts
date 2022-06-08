9febe8dd5aff1ab26928a1b6e8d9f2d636a78333
John Barnes Oduro Twumasi <ohenesetwumasi@gmail.com>
2022-06-08 15:55:33 +0000

    Fix: Mailer" "

--- src/lib/mailer.ts
+++ src/lib/mailer.ts
@@ -1,19 +1,50 @@
 import nodemailer from "nodemailer";
+import { google } from "googleapis";
+const OAuth2 = google.auth.OAuth2;
+const createTransporter = async () => {
+  const oauth2Client = new OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SEC,
+    "https://developers.google.com/oauthplayground"
+  );
 
-const transport = nodemailer.createTransport({
-  service: "Gmail",
-  auth: {
-    user: process.env.MAILER,
-    pass: process.env.MAILPASS,
-  },
-});
+  oauth2Client.setCredentials({
+    refresh_token: process.env.MAIL_REF,
+  });
+
+  const accessToken = await new Promise((resolve, reject) => {
+    oauth2Client.getAccessToken((err, token) => {
+      if (err) {
+        reject("Failed to create access token :(");
+      }
+      resolve(token);
+    });
+  });
+
+  const transporter = nodemailer.createTransport({
+    //@ts-ignore
+    service: "gmail",
+    auth: {
+      serviceClient: "gmail",
+      type: "OAuth2",
+      user: process.env.EMAIL,
+      accessToken,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SEC,
+      refreshToken: process.env.MAIL_REF,
+    },
+  });
+
+  return transporter;
+};
 
 export const sendMail: (
   email: string,
   message: string,
   subject: string
-) => void = (email, message, subject) => {
+) => Promise<void> = async (email, message, subject) => {
   try {
+    let transport = await createTransporter();
     transport.sendMail({
       from: process.env.MAILER,
       to: email,
