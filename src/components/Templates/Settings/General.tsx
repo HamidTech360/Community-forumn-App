bb9196d6d55b194525bebfa29c14d4c99a8f4ef3
Stella Oseyomon <78904383+stellaose@users.noreply.github.com>
2022-06-17 10:34:46 +0100

    profile and settings page padding and margin-fix

--- src/components/Templates/Settings/General.tsx
+++ src/components/Templates/Settings/General.tsx
@@ -8,21 +8,21 @@ const General = () => {
     <>
       <Container className = {styles.container}>
         <Form className = {styles.form}>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Group className={`mb-3 ${styles.group}`} controlId="exampleForm.ControlInput1">
             <Form.Label className = { styles.label}>Full Name</Form.Label>
 
             <Form.Control type="email" placeholder="Mike Dike" className = {styles.input} />
           </Form.Group>
 
 
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Group className={`mb-3 ${styles.group}`} controlId="exampleForm.ControlInput1">
             <Form.Label className = { styles.label}>Username</Form.Label>
 
             <Form.Control type="password" placeholder="Mikengdike" className = { styles.input }/>
           </Form.Group>
 
 
-          <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Group  className={`mb-3 ${styles.group}`}controlId="formBasicEmail">
             <Form.Label className = { styles.label}>Email Address</Form.Label>
 
             <Form.Control type="email" placeholder="Mikedike@gmail.com" className = {styles.input} />
