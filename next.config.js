65e3c469ccc21903577b775152c0278a7e16d33f
John Barnes <ohenesetwumasi@gmail.com>
2022-09-16 14:10:41 +0000

    Fix: next config fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "standalone",
   images: {
-    domains: [
-      "setlinn.s3.us-east-1.amazonaws.com",
-      "lh3.googleusercontent.com"
-    ],
-    output: "standalone"
+    domains: ["setlinn.s3.us-east-1.amazonaws.com", "lh3.googleusercontent.com"]
   }
 };
 
